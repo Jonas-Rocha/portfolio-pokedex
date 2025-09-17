8cdfed79240a0ab29d6949d5a0a10d42f509ab12
Jonas Rocha <jonasrpnstudy@gmail.com>
2025-09-17 19:12:21 -0300

    Tentando colocar um pokemon na tela

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const formSubmit = document.getElementById("form-submit");
 const search = document.getElementById("search");
 const submitButton = document.getElementById("submit");
+const pokeImg = document.getElementById("poke-img");
 
 
 
@@ -8,19 +9,19 @@ formSubmit.addEventListener('submit', async (event) => {
     event.preventDefault();
     const pokemon = search.value.toLowerCase();
     const url = `https://pokeapi.co/api/v2/pokemon/${pokemon}`
-
-
     try {
     const response = await fetch(url);
     if (response.status === 200) {
-    const obj = await response.json();
-    console.log(obj);
+    const pokeObj = await response.json();
+    pokeImg.setAttribute("src", `${pokeObj.sprites.front_default}`);
     }
 
+    
 
-        
-        
-        
+
+
+
+
     } catch (error) {
         console.log("Erro:", error)
     }
