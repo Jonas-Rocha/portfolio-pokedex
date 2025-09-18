54d57c1eacb7b9111193d1d72d9ee96ee6815585
Jonas Rocha <jonasrpnstudy@gmail.com>
2025-09-17 21:01:15 -0300

    apenas dando uma pausa

--- script.js
+++ script.js
@@ -7,13 +7,58 @@ const pokeImg = document.getElementById("poke-img");
 
 formSubmit.addEventListener('submit', async (event) => {
     event.preventDefault();
-    const pokemon = search.value.toLowerCase();
-    const url = `https://pokeapi.co/api/v2/pokemon/${pokemon}`
+    const pokemonValue = search.value.toLowerCase();
+    const url = `https://pokeapi.co/api/v2/pokemon/${pokemonValue}`
+
+
     try {
+    const pokeInitialDescription = document.getElementsByClassName
     const response = await fetch(url);
     if (response.status === 200) {
     const pokeObj = await response.json();
     pokeImg.setAttribute("src", `${pokeObj.sprites.front_default}`);
+
+
+    //Criando a box dos elementos do span [div class new-span-box]
+    const newSpanBox = document.createElement("div");
+    newSpanBox.setAttribute("class", "new-span-box");
+
+    //Criando os spans [span id="type", class="new-span"]
+    const spanType = document.createElement("span");
+    spanType.setAttribute("id", "type");
+    spanType.setAttribute("class", "new-span");
+
+    //Criando os spans [span id="region", class="new-span"]
+    const spanRegion = document.createElement("span");
+    spanRegion.setAttribute("id", "region");
+    spanRegion.setAttribute("class", "new-span");
+
+    //Criando os spans [span id="generation", class="new-span"]
+    const spanGeneration = document.createElement("span");
+    spanGeneration.setAttribute("id", "generation");
+    spanGeneration.setAttribute("class", "new-span");
+
+    //Criando a variável da div pai (onde serão inseridos os spans);
+    const newPokeBox = document.getElementsByClassName("new-poke-box")[0];
+    newPokeBox.append(newSpanBox)[1]
+
+
+
+
+
+    // lembre que tudo isso é dentro do poke display. arrume!
+
+
+
+
+
+
+
+
+
+
+
+
     }
 
     
